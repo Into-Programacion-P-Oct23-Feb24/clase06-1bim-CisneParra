625788dbba01ce60f05c62c6899a5ac26da40616
Cisne Parra <adparra2@utpl.edu.ec>
2023-11-15 15:29:50 -0500

    cambios de la clase

--- ejemplos-switch/EjemplosSwitch/src/manejocadenas/EjemploCadenas.java
+++ ejemplos-switch/EjemplosSwitch/src/manejocadenas/EjemploCadenas.java
@@ -10,13 +10,19 @@ package manejocadenas;
  * @author reroes
  */
 public class EjemploCadenas {
+
     public static void main(String[] args) {
         String ciudad = "loJa";
         ciudad = ciudad.toUpperCase();
         System.out.printf("%s\n", ciudad.toLowerCase());
         System.out.printf("%s\n", ciudad.toUpperCase());
         System.out.printf("%s\n", ciudad);
-        
-        
+        /*
+        -toUpperCase
+        pasa una cadena a mayuscula
+        -toLoweCase
+        pasa una cadena a minúscula
+         */
+
     }
 }
